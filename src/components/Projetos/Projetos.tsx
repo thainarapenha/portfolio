beae2a7a51eb43050e2db6394664dde9160b75b7
thainarapenha <thainarapenha@alu.ufc.br>
2023-02-16 16:10:54 -0300

    chore: removendo um card de projetos

--- src/components/Projetos/Projetos.tsx
+++ src/components/Projetos/Projetos.tsx
@@ -1,5 +1,4 @@
 import React from "react";
-import imgMeuLivro from "../../assets/img/meuLivro.png";
 import imgAdoteAqui from "../../assets/img/adoteAqui.png";
 import imgCursos from "../../assets/img/plataformaCursos.png";
 import imgHomem from "../../assets/img/homem.svg";
@@ -13,12 +12,6 @@ export const Projetos: React.FC = () => {
       </div>
 
       <div className="divCardProjetos">
-        <div className="CardProjetos">
-          <h6>Meu Livro</h6>
-          <a href="https://meu-livro-frontend.vercel.app/" target="_blank">
-            <img src={imgMeuLivro}/>
-          </a>
-        </div>
         <div className="CardProjetos">
           <h6>Adote Aqui</h6>
           <a href="https://thainarapenha.github.io/adoteaqui/" target="_blank">
